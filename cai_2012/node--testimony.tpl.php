58c053e6c5c48be821dcd011037f8e32538a034d
John Fletcher <john@saltwebsites.com>
2016-09-14 20:48:34 +0200

    adapted for vimeo videos

--- cai_2012/node--testimony.tpl.php
+++ cai_2012/node--testimony.tpl.php
@@ -4,7 +4,7 @@
   $first_name_items = field_get_items('node', $node, 'field_author_first_name');
   $first_name = $first_name_items[0]['safe_value'];
   $l10n_first_name_items = field_get_items('node', $node, 'field_l10n_author_first_name');
-  $l10n_first_name = $l10n_first_name_items[0]['safe_value']; 
+  $l10n_first_name = $l10n_first_name_items[0]['safe_value'];
   $qualifications_items = field_get_items('node', $node, 'field_author_qualification');
   $qualifications = $qualifications_items[0]['safe_value'];
   $city_items =  field_get_items('node', $node, 'field_author_city');
@@ -14,7 +14,7 @@
   // author's native language - used in searching for videos
   $author_native_language_items = field_get_items('node', $node, 'field_author_native_language');
   $author_language = $author_native_language_items[0]['safe_value'];
-  
+
   // Media (images, video) relating to this testimony is named after the English node ID.
   if (empty($node->tnid)) {
     // tnid is blank when there are no translations, so use the nid.
@@ -22,7 +22,7 @@
   } else {
     $media_name = $node->tnid;
   }
-  
+
   // build author's name, qualifications, location as formatted string
   // if a localized name was entered, use that
   if ($l10n_first_name) {
@@ -30,14 +30,14 @@
   } else {
     $author_details = $first_name;
   }
-  
+
   if (!empty($qualifications)) {
     $author_details .= ", $qualifications";
-  } 
+  }
   // to temporarily remove the location the following lines have to be commented out
   if (!empty($country)) {
     $author_details .= " - ";
-  } 
+  }
   if (!empty($city)) {
     $author_details .= "$city, ";
    }
@@ -47,7 +47,7 @@
 
   //if only the city should not be mentioned as location, the following lines can be used
   //if (!empty($country)) {
-  //$author_details .= " - $country"; 
+  //$author_details .= " - $country";
   //}
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
@@ -56,7 +56,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if (!$page):
-      // print title and video image if applicable. The reason for some of the strange separation of 
+      // print title and video image if applicable. The reason for some of the strange separation of
       // tags/angle-brackets over lines is so that a space is not created by the line break ?>
       <h2<?php print $title_attributes; ?>
         ><a href="<?php print $node_url; ?>"><?php
@@ -77,10 +77,14 @@
     <?php if ($page) {
       drupal_add_css(path_to_theme() . '/css/testimony.css');
 
-      // Second param is whether to preload video, i.e. if teaser don't preload because there are heaps on one page 
-      print cai_2012_html5_video($node, $page);
+      $video_markup = cai_2012_vimeo_video($node);
+      if (empty($video_markup)) {
+        // Second param is whether to preload video, i.e. if teaser don't preload because there are heaps on one page
+        $video_markup = cai_2012_html5_video($node, $page);
+      }
+      print $video_markup;
     ?>
-  
+
       <div class="testimony-pics">
         <img src="/files/images/testimonies/<?php print $media_name ?>-1.jpg" alt="<?php print t('Photo of @name', array('@name' => $first_name)); ?>" width="300" class="first"/>
         <?php // add any additional photos (maximum 8)
@@ -95,7 +99,7 @@
           }
         } ?>
       </div>
-  <?php 
+  <?php
     } // end if ($page)
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
@@ -106,7 +110,7 @@
   <?php
   if ($page) {
     print render($content['links']);
-      
+
       // Get taxonomy tags that this node is tagged with. Taxonomy vocabularies are treated like fields.
       $terms = field_view_field('node', $node, 'taxonomy_vocabulary_2');
       foreach ($terms['#items'] as $item) {
@@ -115,8 +119,8 @@
       <?php print views_embed_view('ordered_taxonomy_term', 'block', $term->tid, $node->nid);
     }
   }
-  
-  print render($content['comments']); 
+
+  print render($content['comments']);
   ?>
 
 </div>
